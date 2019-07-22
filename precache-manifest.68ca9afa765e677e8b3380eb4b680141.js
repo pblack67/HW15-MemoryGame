5716b0b8b9bd67f0a20f7811d81641bf379758e7
Paul Black <pblack67@comcast.net>
2019-07-22 15:00:00 -0500

    Updates

---
+++ precache-manifest.68ca9afa765e677e8b3380eb4b680141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff67d834be8d695bad58c15313d6780c",
+    "url": "/HW15-MemoryGame/index.html"
+  },
+  {
+    "revision": "52c5fd867b769f4a78b2",
+    "url": "/HW15-MemoryGame/static/css/main.ba37ad92.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/HW15-MemoryGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "52c5fd867b769f4a78b2",
+    "url": "/HW15-MemoryGame/static/js/main.ad0963d8.chunk.js"
+  },
+  {
+    "revision": "9e0fc15b5826aa8ef6b7",
+    "url": "/HW15-MemoryGame/static/js/runtime~main.834f9d1b.js"
+  }
+]);
